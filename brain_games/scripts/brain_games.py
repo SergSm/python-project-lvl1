d5f6622ed0ad7d1c461d6ab0cc750a1907289d8d
SergeyS <smirnovserg.s@gmail.com>
2020-06-14 20:30:52 +0600

    flake8 fxs

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,13 @@
+"""main module."""
+
 from .cli import welcome_user
 
+
 def main():
-    print("Welcome to the Brain Games!")
+    """Print data."""
+    print('Welcome to the Brain Games!')
     welcome_user()
-    
+
 
 if __name__ == '__main__':
     main()
--- brain_games/scripts/cli.py
+++ brain_games/scripts/cli.py
@@ -1,5 +1,9 @@
+"""User unput module."""
+
 import prompt
 
+
 def welcome_user():
+    """Greet user and ask to input."""
     name = prompt.string('May I have your name? ')
-    print('Hello,'+name)
+    print('Hello,', name)
