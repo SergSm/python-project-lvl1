18c5d6a70d6a220b14973e1391f3166e30cb950a
SergeyS <smirnovserg.s@gmail.com>
2020-08-24 14:42:13 +0600

    new lines

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,5 +1,6 @@
 """brain gcd games run script"""
 
+
 from brain_games.engine.main import start_game
 from brain_games.engine.games import brain_gcd
 
--- brain_games/scripts/cli.py
+++ brain_games/scripts/cli.py
@@ -1,32 +1,9 @@
 """Command line input module."""
 
+
 import prompt
 
-
-#############################################
 
 def ask(question):
     user_answer = prompt.string(question)
     return user_answer
-
-#############################################
-
-
-# def welcome_user():
-#     """Ask to input name and greet user."""
-#     username = ask_for_username()
-#     print('Hello,', username)
-#     return username
-#
-#
-# # TODO: 2 eradicate
-# def ask_for_username():
-#     name = prompt.string('May I have your name? ')
-#     return name
-#
-#
-# # TODO: 3 eradicate
-# def ask_for_user_input():
-#     """User input"""
-#     user_answer = prompt.string('Your answer: ')
-#     return user_answer
